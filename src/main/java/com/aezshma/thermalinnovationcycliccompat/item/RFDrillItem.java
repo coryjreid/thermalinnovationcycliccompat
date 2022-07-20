44add7044209a7b3dd1ef0e370ea7454b8640055
Cory J Reid <cjr@coryjreid.com>
2022-07-19 20:32:42 -0600

    Ensure We Report We Support Cyclic's Enchant
    Previously we never ensured our addition to VALID_ENCHANTS was called
    and executed. We also never fully stated (via method override) that
    we're allowed to accept the enchantment.

--- src/main/java/com/aezshma/thermalinnovationcycliccompat/item/RFDrillItem.java
+++ src/main/java/com/aezshma/thermalinnovationcycliccompat/item/RFDrillItem.java
@@ -2,13 +2,22 @@ package com.aezshma.thermalinnovationcycliccompat.item;
 
 import com.lothrazar.cyclic.ModCyclic;
 import com.lothrazar.cyclic.enchant.EnchantXp;
+
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class RFDrillItem extends cofh.thermal.innovation.item.RFDrillItem {
     public RFDrillItem(Item.Properties builder, int maxEnergy, int maxTransfer) {
         super(builder, maxEnergy, maxTransfer);
+        setupEnchants();
+    }
+
+    @Override
+    public boolean canApplyAtEnchantingTable(final ItemStack stack, final Enchantment enchantment) {
+        return super.canApplyAtEnchantingTable(stack, enchantment) || enchantment instanceof EnchantXp;
     }
 
     public static void setupEnchants() {
--- src/main/java/com/aezshma/thermalinnovationcycliccompat/item/RFSawItem.java
+++ src/main/java/com/aezshma/thermalinnovationcycliccompat/item/RFSawItem.java
@@ -2,12 +2,21 @@ package com.aezshma.thermalinnovationcycliccompat.item;
 
 import com.lothrazar.cyclic.ModCyclic;
 import com.lothrazar.cyclic.enchant.EnchantXp;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class RFSawItem extends cofh.thermal.innovation.item.RFSawItem {
     public RFSawItem(Properties builder, int maxEnergy, int maxTransfer) {
         super(builder, maxEnergy, maxTransfer);
+        setupEnchants();
+    }
+
+    @Override
+    public boolean canApplyAtEnchantingTable(final ItemStack stack, final Enchantment enchantment) {
+        return super.canApplyAtEnchantingTable(stack, enchantment) || enchantment instanceof EnchantXp;
     }
 
     public static void setupEnchants() {
